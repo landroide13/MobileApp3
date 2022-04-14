838731f1fd170275f95014552140ee41e766dddf
landroide13 <landrade1313@gmail.com>
2022-04-14 16:00:53 +1200

    sqilte

--- MerchanServ/Model/Merchandiser.cs
+++ MerchanServ/Model/Merchandiser.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using SQLite;
+
 namespace MerchanServ.Model
 {
     public class Merchandiser
     {
+        [PrimaryKey]
+        public int ID { get; set; }
+
         public string Name { get; set; }
 
         public string Phone { get; set; }
--- MerchanServ/Model/Ticket.cs
+++ MerchanServ/Model/Ticket.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using SQLite;
+
 namespace MerchanServ.Model
 {
     public class Ticket
     {
+        [PrimaryKey]
+        public int ID { get; set; }
+
         public int time { get; set; }
 
         public Ticket()
