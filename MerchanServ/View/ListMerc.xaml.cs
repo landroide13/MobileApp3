88d971936605bf2bf47241631d1183326776de89
landroide13 <landrade1313@gmail.com>
2022-04-16 17:08:45 +1200

    navigation supervisor

---
+++ MerchanServ/View/ListMerc.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MerchanServ.View
+{
+    public partial class ListMerc : ContentPage
+    {
+        public ListMerc()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- MerchanServ/View/MenuSuperv.xaml.cs
+++ MerchanServ/View/MenuSuperv.xaml.cs
@@ -11,5 +11,32 @@ namespace MerchanServ.View
         {
             InitializeComponent();
         }
+
+        async void Button_AddMerc(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new AddMerchandiser());
+        }
+
+        async void Button_AddStore(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new AddStore());
+        }
+
+        async void Button_AddTicket(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new AddTicket());
+        }
+
+        async void Button_ListTicket(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new ListTickets());
+        }
+
+        async void Button_ListMerc(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new ListMerc());
+        }
+
+
     }
 }
