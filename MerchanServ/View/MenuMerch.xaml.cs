d3fee85ddc9a6c7d18f7bc217239ebf031cdfe8f
Luis Alfonso Andrade Reuss <20210219@learn.cpp.ac.nz>
2022-04-20 09:11:52 +1200

    Merch Menu

--- MerchanServ/View/MenuMerch.xaml.cs
+++ MerchanServ/View/MenuMerch.xaml.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.IO;
+using SQLite;
 using Xamarin.Forms;
 
 namespace MerchanServ.View
 {
     public partial class MenuMerch : ContentPage
     {
+
+        string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "MerchanServDB.db3");
+
+        Model.Ticket tic = new Model.Ticket();
+
         public MenuMerch()
         {
             InitializeComponent();
+
+            var db = new SQLiteConnection(dbPath);
+
+            listTickets.ItemsSource = db.Table<Model.Ticket>().OrderBy(t => t.TicDate).ToList();
+
+            listTickets.ItemSelected += Listview_ItemSelected;
         }
+
+
+        private void Listview_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            tic = (Model.Ticket)e.SelectedItem;
+           
+        }
+
     }
 }
