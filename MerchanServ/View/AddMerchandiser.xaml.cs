ccdfe3b8289c007a6abb63887b2828c121fefb52
Luis Alfonso Andrade Reuss <20210219@learn.cpp.ac.nz>
2022-04-20 09:28:49 +1200

    Merchandiser Add

--- MerchanServ/View/AddMerchandiser.xaml.cs
+++ MerchanServ/View/AddMerchandiser.xaml.cs
@@ -1,15 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.IO;
+using SQLite;
 using Xamarin.Forms;
 
 namespace MerchanServ.View
 {
     public partial class AddMerchandiser : ContentPage
     {
+        string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "MerchanServDB.db3");
+
+
         public AddMerchandiser()
         {
             InitializeComponent();
+        }
+
+         async void Button_AddMerc(System.Object sender, System.EventArgs e)
+        {
+            var db = new SQLiteConnection(dbPath);
+
+            db.CreateTable<Model.Merchandiser>();
+
+            var pKey = db.Table<Model.Merchandiser>().OrderByDescending(c => c.ID).FirstOrDefault();
+
+            Model.Merchandiser mer = new Model.Merchandiser
+            {
+                ID = pKey == null ? 1 : pKey.ID + 1,
+                Name = merName.Text,
+                Phone = merAddress.Text,
+            };
+
+            db.Insert(mer);
+
+            await DisplayAlert(null, mer.Name + " " + "Added", "OK");
+            await Navigation.PopAsync();
         }
     }
 }
